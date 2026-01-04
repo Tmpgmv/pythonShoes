07b704616dd40754981e50194033c02fb4ade874
Your Name <you@example.com>
2026-01-04 13:20:44 +0300

    tmp

--- shoes/settings.py
+++ shoes/settings.py
@@ -85,17 +85,24 @@ WSGI_APPLICATION = 'shoes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+        # "ENGINE": "django.db.backends.postgresql",
+        # "NAME": "pythonshoes",
+        # "USER": "pythonshoes",
+        # "PASSWORD": "pythonshoes",
+        # "HOST": "127.0.0.1",
+        # "PORT": "5432",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "pythonshoes",
-        "USER": "pythonshoes",
-        "PASSWORD": "pythonshoes",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -132,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = "/var/www/example.com/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',   # where your bootstrap.min.css actually is
